daf3a225f456061f65e74964079cd0981ec105fe
Goncalves De Queiroz <bgoncalvesq@indracompany.com>
2019-10-10 23:25:47 -0300

    Ajuste no codigo

--- code_automatic_irrigation_system/code_automatic_irrigation_system.ino
+++ code_automatic_irrigation_system/code_automatic_irrigation_system.ino
@@ -1,20 +1,15 @@
 /*
  * Autor: Bruno Queiroz
  * Data: 11/09/2019
- * Função: Programa responsavel por ficar monitorando o solo onde estar a planta,
- *         monitorar o nivel da água do reservatorio para acionar a bomba d'água 
- *         junto com os leds que funcionará como sinal para o usuário se o reservatorio
- *         precisa de mais água ou não.
+ * Função: Programa responsável por monitorar o solo, para acionar a bomba d'água 
+ *         e os LEDs funcionarão como sinal de estado do solo.
 */
 
 #define analog_sinal_pin A0
-#define analog_reservoir_pin A1
 #define pin_led_red 4
 #define pin_led_green 5
 #define pin_water_pump 6
-#define pin_reservoir_limit 7
 
-int reservoir_sensor;
 int analog_value;
 
 void setup() {
@@ -29,8 +24,24 @@ void loop() {
   //Realiza a monitoração do pino A0 do sensor
   analog_value = analogRead(analog_sinal_pin);
 
+  //Mostra o valor da porta analogica no serial monitor
+  Serial.print("Porta analogica: ");
+  Serial.print(analog_value);
+
+  //Desliga os LEDs
+  turnOffLeds();
+  
   //Verifica solo não esta umido, para acende
-  if(analog_value > 800 && analog_value < 1024){    
+  if(analog_value > 0 && analog_value < 400){
+    Serial.println(" Status: Solo umido");
+    digitalWrite(pin_led_green, HIGH);  
+    digitalWrite(pin_water_pump, LOW);
+  }
+  
+  //Verifica solo não esta umido, para acende
+  if(analog_value > 800 && analog_value < 1024){
+    Serial.println(" Status: Solo seco");
+    digitalWrite(pin_led_red, HIGH);        
     digitalWrite(pin_water_pump, HIGH);
   }
   
@@ -41,27 +52,4 @@ void turnOffLeds()
 {
   digitalWrite(pin_led_red, LOW);
   digitalWrite(pin_led_green, LOW);  
-}
-
-void turnOnLeds()
-{
-  digitalWrite(pin_led_red, HIGH);
-  digitalWrite(pin_led_green, HIGH);  
-}
-
-void turnOnSensors() {
-  digitalWrite(analog_sinal_pin,HIGH);
-  digitalWrite(analog_reservoir_pin,HIGH); 
-}
-
-void turnOffSensors() {
-  digitalWrite(analog_sinal_pin,LOW);
-  digitalWrite(analog_reservoir_pin,LOW); 
-}
-
-int readReservoirSensor() {
-  turnOnSensors();
-  delay(10);
-  return analogRead(analog_reservoir_pin);
-  turnOffSensors();
 }
